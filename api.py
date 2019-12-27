aa3d3356cb65257e6e61dd14bdf3566bdf9aee56
Danya Generalov <danya@danya02.ru>
2019-12-28 02:12:22 +0300

    Adding upload-to-profile function.

--- api.py
+++ api.py
@@ -5,6 +5,6 @@ if SECRETS.token:
     vk_session = vk_api.VkApi(SECRETS.login, token=SECRETS.token)
 else:
     vk_session = vk_api.VkApi(SECRETS.login, SECRETS.password)
-vk_session.auth()
+    vk_session.auth()
 
 vk = vk_session.get_api()
