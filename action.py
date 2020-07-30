13519e1d9d232a4e9e372ea64fdcecf8e497194c
Danya Generalov <danya@danya02.ru>
2020-07-30 03:39:04 +0300

    Adding action to add story.

--- action.py
+++ action.py
@@ -2,6 +2,8 @@ import upload
 import wall_post
 import random_images
 import random
+import stories
+import api
 
 def update_profile_picture():
     picture = random_images.get_any()
@@ -11,8 +13,11 @@ def post_wall_picture():
     picture = random_images.get_any()
     wall_post.post_one_image(picture)
 
+def post_story():
+    stories.post_images(api.vk, random.randint(1, 3))
+
 def do_anything():
-    actions = [update_profile_picture, post_wall_picture]
+    actions = [update_profile_picture, post_wall_picture, post_story]
     this_action = random.choice(actions)
     this_action()
 
